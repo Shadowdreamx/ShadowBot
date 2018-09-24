c1e3804ecd58985f3280d43459edcb29c9cd5fb3
Shadowdreamx <43547447+Shadowdreamx@users.noreply.github.com>
2018-09-24 18:13:52 -0300

    Update shadow.js

--- shadow.js
+++ shadow.js
@@ -79,20 +79,6 @@ if(command === 'user'){
 
   }
 
-let miembro = message.mentions.members.first();
-let nombrerol = args.split(' ').slice(1).join(' ');
-
-let role = message.guild.roles.find("name", nombrerol);
-let perms = message.member.hasPermission("MANAGE_ROLES_OR_PERMISSIONS");
-
-if(!perms) return message.channel.send("`Error` `|` No tienes Permisos para usar este comando.");
-     
-if(message.mentions.users.size < 1) return message.reply('Debe mencionar a un miembro.').catch(console.error);
-if(!nombrerol) return message.channel.send('Escriba el nombre del rol a agregar.');
-if(!role) return message.channel.send('Rol no encontrado en el servidor.');
-    
-miembro.addRole(role).catch(console.error);
-message.channel.send(`El rol **${role.name}** fue agregado correctamente a **${miembro.user.username}**.`);
   
 });
 client.login(config.token);
